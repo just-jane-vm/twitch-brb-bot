ae3939a05fbec808e636ec86084edfc6927a59cc
yumichi <yubin.yumiko.ichikawa@gmail.com>
2025-03-04 21:08:20 -0500

    Add guard for commands and detach when not in use

--- src/brbbot.py
+++ src/brbbot.py
@@ -7,10 +7,21 @@ from game import Game
 import asyncio
 import multiprocessing
 from logger import get_logger
+from functools import wraps
 
 SCOPES = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
 class BRBBot():
+    def event_ch_guard(func):
+        @wraps(func)
+        async def wrapper(self, *args, **kwargs):
+            if self.event_ch:
+                return await func(self, *args, **kwargs)
+            else:
+                self.logger.error("Event channel guard prevented access to null event channel.")
+                return None
+        return wrapper
+
     def __init__(self):
         self.config = Config()
         self.game = None
@@ -37,6 +48,18 @@ class BRBBot():
         if (self.config.twitch.commands.stop):
             self.chat.register_command(self.config.twitch.commands.stop, self.on_stop)
 
+        try:
+            self.chat.start()
+            input('press ENTER to stop\n')
+        finally:
+            # now we can close the chat bot and the twitch api client
+            if self.game_process:
+                self.kill_ch.send("die")
+
+            self.chat.stop()
+            await self.twitch.close()
+
+    def register_commands(self):
         if (self.config.twitch.commands.add_user):
             self.chat.register_command(self.config.twitch.commands.add_user, self.on_add)
 
@@ -49,23 +72,17 @@ class BRBBot():
         if (self.config.twitch.add_user_on_msg):
             self.chat.register_event(ChatEvent.MESSAGE, self.on_message)
 
-        self.chat.start()
-
-        try:
-            input('press ENTER to stop\n')
-        finally:
-            # now we can close the chat bot and the twitch api client
-            if self.game_process:
-                self.kill_ch.send("die")
-
-            self.chat.stop()
-            await self.twitch.close()
+    def unregister_commands(self):
+        self.chat.unregister_command(self.config.twitch.commands.add_user)
+        self.chat.unregister_command(self.config.twitch.commands.speed_upp)
+        self.chat.unregister_command(self.config.twitch.commands.slow_down)
+        self.chat.unregister_event(ChatEvent.MESSAGE)
 
     def _start_game(self, ch1, ch2, config):
-        self.logger.debug("Game process start")
+        self.logger.info("Game process start")
         game = Game(config, ch1, ch2)
         game.run()
-        self.logger.debug("Game process exit")
+        self.logger.info("Game process exit")
 
     def _stop_game(self):
         if not self.game_process:
@@ -81,10 +98,6 @@ class BRBBot():
         self.event_ch = None
         self.logger.debug("Game killed")
 
-    async def on_message(self, msg: ChatMessage):
-        if (self.event_ch):
-            self.event_ch.send(f'cmd=add {msg.user.display_name},{msg.user.color}')
-
     async def stop(self):
         self.chat.stop()
         await self.twitch.close()
@@ -95,23 +108,28 @@ class BRBBot():
             await ready_event.chat.join_room(channel)
             self.logger.debug(f'Joined [{channel}] succesfully.')
 
+    @event_ch_guard
+    async def on_message(self, msg: ChatMessage):
+        self.event_ch.send(f'cmd=add {msg.user.display_name},{msg.user.color}')
+
+    @event_ch_guard
     async def on_add(self, cmd: ChatCommand):
-        if (self.event_ch):
-            self.event_ch.send(f'cmd=add {cmd.user.display_name},{cmd.user.color}')
+        self.event_ch.send(f'cmd=add {cmd.user.display_name},{cmd.user.color}')
+
+    @event_ch_guard
+    async def on_speed_up(self, cmd: ChatCommand):
+        self.event_ch.send('cmd=speed_up')
+
+    @event_ch_guard
+    async def on_slow_down(self, cmd: ChatCommand):
+        self.event_ch.send('cmd=slow_down')
 
     async def on_stop(self, cmd: ChatCommand):
         if cmd.user.name != self.config.twitch.channel_names:
             return
 
         self._stop_game()
-
-    async def on_speed_up(self, cmd: ChatCommand):
-        if (self.event_ch):
-            self.event_ch.send('cmd=speed_up')
-
-    async def on_slow_down(self, cmd: ChatCommand):
-        if (self.event_ch):
-            self.event_ch.send('cmd=slow_down')
+        self.unregister_commands()
 
     async def on_start(self, cmd: ChatCommand):
         if cmd.user.name not in self.config.twitch.channel_names:
@@ -124,7 +142,8 @@ class BRBBot():
             self.logger.debug("Starting game...")
             self.game_process.start()
             self.logger.debug("Game started...")
-
+            self.register_commands()
+ 
 async def main():
     bot = BRBBot()
     await bot.start()
